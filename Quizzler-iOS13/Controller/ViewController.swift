73880c1ad2a19ab0a7981c61ee83027f3379738d
Aiden <nagyeong.yeo1023@gmail.com>
2022-02-28 23:29:38 +0900

    MVC pattern with mutating keyword

--- Quizzler-iOS13/Controller/ViewController.swift
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -15,22 +15,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var falseButton: UIButton!
     @IBOutlet weak var progressBar: UIProgressView!
     
-    let quiz = [
-        Quiz(q: "A slug's blood is green.", a: "True"),
-        Quiz(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
-        Quiz(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
-        Quiz(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
-        Quiz(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
-        Quiz(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
-        Quiz(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
-        Quiz(q: "Google was originally called 'Backrub'.", a: "True"),
-        Quiz(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
-        Quiz(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
-        Quiz(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
-        Quiz(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
-    ]
-    
-    var questionNumber = 0
+    var quizBrain = QuizBrain()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -41,28 +26,28 @@ class ViewController: UIViewController {
 
     @IBAction func answerButtonPressed(_ sender: UIButton) {
         
-        let userAnswer = sender.currentTitle
-        if userAnswer == quiz[questionNumber].answer {
+        let userAnswer = sender.currentTitle!
+        let userGotitRight = quizBrain.checkAnswer(userAnswer) // readable code!
+        
+        if userGotitRight {
             sender.backgroundColor = .green
         } else {
             sender.backgroundColor = .red
         }
         
-        if questionNumber < quiz.count - 1 {
-            questionNumber += 1
-        } else {
-            questionNumber = 0
-        }
+        quizBrain.nextQuestion()
         
         Timer.scheduledTimer(timeInterval: 0.2, target: self, selector: #selector(updateUI), userInfo: nil, repeats: false)
         
     }
     
     @objc func updateUI() {
-        questionText.text = quiz[questionNumber].title
+        questionText.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        
         trueButton.backgroundColor = .clear
         falseButton.backgroundColor = .clear
-        progressBar.progress = Float(questionNumber + 1) / Float(quiz.count)
+        
     }
     
     
--- Quizzler-iOS13/Model/QuizBrain.swift
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -10,4 +10,44 @@ import Foundation
 
 struct QuizBrain {
     
+    let quiz = [
+        Quiz(q: "A slug's blood is green.", a: "True"),
+        Quiz(q: "Approximately one quarter of human bones are in the feet.", a: "True"),
+        Quiz(q: "The total surface area of two human lungs is approximately 70 square metres.", a: "True"),
+        Quiz(q: "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.", a: "True"),
+        Quiz(q: "In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.", a: "False"),
+        Quiz(q: "It is illegal to pee in the Ocean in Portugal.", a: "True"),
+        Quiz(q: "You can lead a cow down stairs but not up stairs.", a: "False"),
+        Quiz(q: "Google was originally called 'Backrub'.", a: "True"),
+        Quiz(q: "Buzz Aldrin's mother's maiden name was 'Moon'.", a: "True"),
+        Quiz(q: "The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.", a: "False"),
+        Quiz(q: "No piece of square dry paper can be folded in half more than 7 times.", a: "False"),
+        Quiz(q: "Chocolate affects a dog's heart and nervous system; a few ounces are enough to kill a small dog.", a: "True")
+    ]
+    
+    var questionNumber = 0
+    
+    func checkAnswer(_ userAnswer: String) -> Bool {
+        if userAnswer == quiz[questionNumber].answer {
+            return true
+        } else {
+            return false
+        }
+    }
+    
+    func getProgress() -> Float {
+        return Float(questionNumber + 1) / Float(quiz.count)
+    }
+    
+    func getQuestionText() -> String {
+        return quiz[questionNumber].title
+    }
+    
+    mutating func nextQuestion() {
+        if questionNumber < quiz.count - 1 {
+            questionNumber += 1
+        } else {
+            questionNumber = 0
+        }
+    }
 }
