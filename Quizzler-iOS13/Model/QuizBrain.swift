f6ff77475052b81b58022fb167b8e218eafefe2d
Aiden <nagyeong.yeo1023@gmail.com>
2022-02-28 21:55:00 +0900

    Structure of MVC

---
+++ Quizzler-iOS13/Model/QuizBrain.swift
@@ -0,0 +1,13 @@
+//
+//  QuizBrain.swift
+//  Quizzler-iOS13
+//
+//  Created by 여나경 on 2022/02/28.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct QuizBrain {
+    
+}
